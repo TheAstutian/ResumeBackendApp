695af374dbca47d9add983c3852ae29289a551e2
TheAstutian <speakingatoms@gmail.com>
2021-08-20 17:09:59 +0100

    production config

--- ResumeApp/ResumeApp/settings.py
+++ ResumeApp/ResumeApp/settings.py
@@ -140,8 +140,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
  
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
