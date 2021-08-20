31bcc24fd4db9e769498adbfff1815bd689dc180
TheAstutian <speakingatoms@gmail.com>
2021-08-20 19:36:01 +0100

    Add release phase to procfile

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
+release: python manage.py migrate
 web: gunicorn ResumeApp.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
--- ResumePage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import ResumePage, Responses
-
-urlpatterns = [
-    path('', ResumePage.as_view(), name='index'),
-    path('responses/', Responses.as_view(), name='responses'), 
-]
